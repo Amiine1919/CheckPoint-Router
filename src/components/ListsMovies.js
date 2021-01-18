3968477ec25bb6a0ee91906d59e8399d93e0ce08
Amine Fathallah <fathala.amine@gmail.com>
2021-01-18 22:56:06 +0100

    react-router

--- src/components/ListsMovies.js
+++ src/components/ListsMovies.js
@@ -1,29 +1,32 @@
-
-export const ListsMovies =[{
-    id:Math.random(),
-    title:"Avengers",
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    posterUrl:"https://images-na.ssl-images-amazon.com/images/I/719SFBdxRtL._AC_SL1000_.jpg",
-    rate:4,
-    trailler:"https://www.youtube.com/embed/hA6hldpSTF8"
-
-},
-{
-    id:Math.random(),
-    title:"Fast and Furious 8",
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    posterUrl:"https://images-na.ssl-images-amazon.com/images/I/51wUNfZfAuL._AC_.jpg",
-    rate:5,
-    trailler:"https://www.youtube.com/embed/uisBaTkQAEs"
-
-},
-{
-    id:Math.random(),
-    title:"The lion King",
-    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    posterUrl:"https://images-na.ssl-images-amazon.com/images/I/715DGa4kWVL._AC_SL1000_.jpg",
-    rate:4,
-    trailler:"https://www.youtube.com/embed/7TavVZMewpY"
-
-}
-]
+export const ListsMovies = [
+  {
+    id: Math.random(),
+    title: "Wicker Park",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/51hHoo8F9cL._AC_.jpg",
+    rate: 5,
+    trailler: "https://www.youtube.com/embed/Dy99aOBE2RU",
+  },
+  {
+    id: Math.random(),
+    title: "Inception",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/81p%2Bxe8cbnL._SY445_.jpg",
+    rate: 4,
+    trailler: "https://www.youtube.com/embed/8hP9D6kZseM",
+  },
+  {
+    id: Math.random(),
+    title: "Mad max",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/519QKrYfV1L._SX342_.jpg",
+    rate: 2,
+    trailler: "https://www.youtube.com/embed/hEJnMQG9ev8",
+  },
+];
